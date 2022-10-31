f9384695c6f7740907339bd1d85e4c5f5a4f254c
Qmzik <nikita.lebedev20812@gmail.com>
2022-10-31 14:03:31 +0400

    first commit

---
+++ components/base/BaseButton/BaseButton.tsx
@@ -0,0 +1,66 @@
+import React, {
+    CSSProperties,
+    FC,
+    MouseEvent,
+    PropsWithChildren,
+    useCallback,
+} from 'react';
+import cn from 'classnames';
+
+import c from './baseButton.module.scss';
+
+interface IProps extends PropsWithChildren {
+    loading?: boolean;
+    type?: 'submit' | 'button' | 'reset';
+    disabled?: boolean;
+    onClick?: () => void | Promise<void>;
+    className?: string;
+    style?: CSSProperties;
+    stopPropagation?: boolean;
+    preventDefault?: boolean;
+}
+
+const BaseButton: FC<IProps> = ({
+    loading,
+    type = 'button',
+    disabled,
+    onClick,
+    children,
+    className,
+    stopPropagation,
+    style,
+    preventDefault,
+}) => {
+    const handleClick = useCallback(
+        (e: MouseEvent<HTMLButtonElement>) => {
+            if (stopPropagation) {
+                e.stopPropagation();
+            }
+
+            if (preventDefault) {
+                e.preventDefault();
+            }
+
+            if (loading || !onClick) {
+                return;
+            }
+
+            void onClick();
+        },
+        [loading, onClick, preventDefault, stopPropagation],
+    );
+
+    return (
+        <button
+            type={type}
+            onClick={handleClick}
+            className={cn(c.container, className)}
+            style={style}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default BaseButton;

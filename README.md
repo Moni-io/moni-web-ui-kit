5c1a4e3b0e3b67533930d48dd4bd0d0338b1fb1b
Qmzik <nikita.lebedev20812@gmail.com>
2022-11-01 15:17:20 +0400

    base loader

---
+++ README.md
@@ -0,0 +1,7 @@
+if using next.js add to next.config.js
+```js
+const withTM = require('next-transpile-modules')(['moni-web-ui-kit']);
+
+module.exports = withTM({});
+```
+yarn add -D @types/qs
--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,5 @@
 export { BaseButton } from './base/BaseButton';
+export { BaseLoader } from './base/BaseLoader';
+
 export { ModalContentLayout } from './common/ModalContentLayout';
 export { ModalContentHandler } from './common/ModalContentHandler';

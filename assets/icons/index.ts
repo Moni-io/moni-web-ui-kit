c23b57330a4482a58262f2cd971ef6a60cb00f5d
Qmzik <nikita.lebedev20812@gmail.com>
2022-11-02 16:36:28 +0400

    ChangeArrowLarge12x18Icon

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -1,2 +1,3 @@
 export { default as QuestionMark20x20Icon } from './questionMark.svg';
 export { default as SortTitleArrow11x20Icon } from './sortTitleArrow11x20.svg';
+export { default as ChangeArrowLarge12x18Icon } from './changeArrowLarge12x18.svg';

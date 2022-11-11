845058b72008951211e4dac1603da5cb7505fe1a
Qmzik <nikita.lebedev20812@gmail.com>
2022-11-11 13:57:10 +0400

    IResettablePresenter

--- types/index.ts
+++ types/index.ts
@@ -4,4 +4,7 @@ export type {
     IPagination,
 } from './repositories/interfaces';
 export { RepositoryResultStatus } from './repositories/enums';
+
+export { IResettablePresenter } from './presenters/interfaces';
+
 export { OrderDirectionType } from './entities/enums';
---
+++ types/presenters/interfaces.ts
@@ -0,0 +1,3 @@
+export interface IResettablePresenter {
+    reset: () => void;
+}

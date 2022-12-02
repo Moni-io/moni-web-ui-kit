ec69bad9fdc3be5a34c8f6c55575d904ad17a06a
Qmzik <nikita.lebedev20812@gmail.com>
2022-12-02 11:55:29 +0400

    Close8x8Icon

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -2,3 +2,4 @@ export { default as QuestionMark20x20Icon } from './questionMark.svg';
 export { default as SortTitleArrow11x20Icon } from './sortTitleArrow11x20.svg';
 export { default as ChangeArrowLarge12x18Icon } from './changeArrowLarge12x18.svg';
 export { default as ExternalLink20x20Icon } from './externalLink20x20.svg';
+export { default as Close8x8Icon } from './close8x8.svg';

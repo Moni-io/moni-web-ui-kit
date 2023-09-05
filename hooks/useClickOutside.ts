cb0bcf9047218da3984a71b9bd0c86d22a0b4319
Qmzik <nikita.lebedev20812@gmail.com>
2023-09-05 17:12:00 +0500

    useClickOutsideUpdate

--- hooks/useClickOutside.ts
+++ hooks/useClickOutside.ts
@@ -1,34 +1,31 @@
-import { MutableRefObject, useEffect } from 'react';
+import { MutableRefObject, useEffect, useRef } from 'react';
 
 export const useClickOutside = (
     refs: MutableRefObject<HTMLElement | null>[],
     handler: () => void,
-    isActive = true,
 ): void => {
+    const handleClickOutside = useRef((event: any) => {
+        const isOutside = refs.every(
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+            (ref) => !ref.current?.contains(event.target),
+        );
+
+        if (isOutside) {
+            handler();
+        }
+    });
+
     useEffect(() => {
-        const handleClickOutside = (event: any) => {
-            if (!isActive) {
-                return;
-            }
-
-            const isOutside = refs.every(
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-                (ref) => !ref.current?.contains(event.target),
-            );
-
-            if (isOutside) {
-                handler();
-            }
-        };
+        const func = handleClickOutside.current;
 
         setTimeout(() => {
-            document.addEventListener('click', handleClickOutside);
-            document.addEventListener('dragstart', handleClickOutside);
+            document.addEventListener('click', func);
+            document.addEventListener('dragstart', func);
         });
 
         return () => {
-            document.removeEventListener('click', handleClickOutside);
-            document.removeEventListener('dragstart', handleClickOutside);
+            document.removeEventListener('click', func);
+            document.removeEventListener('dragstart', func);
         };
-    }, [handler, isActive, refs]);
+    }, [handler, refs]);
 };
